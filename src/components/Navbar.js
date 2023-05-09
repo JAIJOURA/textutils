709fc8ea5c3423cc808eb3c17407f2637c77bb46
JAIJOURA <111792753+JAIJOURA@users.noreply.github.com>
2023-05-09 15:35:30 +0530

    finalizing the project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { Link } from 'react-router-dom'
+
 
 export default function Navbar(props) {
   return (
@@ -13,17 +13,11 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">{props.home}</a>
+          <a className="nav-link" aria-current="page" href="#">{props.home}</a>
         </li>
-        {/* <li className="nav-item">
-          <a className="nav-link" href="#">{props.about}</a>
-        </li>  */}
+
       </ul>
-      {/* <div className="d-flex">
-        <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height:'30px',width:'30px' ,color:'primary'}}></div>
-        <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height:'30px',width:'30px' ,color:'primary'}}></div>
-        <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height:'30px',width:'30px' ,color:'primary'}}></div>
-      </div> */}
+      
         <div className={`form-check form-switch text-${props.mode ==='light'?'dark':'light'}`}>
         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
